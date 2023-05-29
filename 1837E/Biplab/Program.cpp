e85597dc9d92f244e441db042d17288719555e39
Biplab Roy <roybiplabofficial@gmail.com>
2023-05-29 11:09:40 +0530

    adding question

---
+++ 1837E/Biplab/Program.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long
+#define sz(v) (int)v.size()
+#define all(v) v.begin(),v.end()
+#define ff first
+#define ss second
+#define pb push_back
+#define endl "\n"
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);srand(time(NULL))
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+
+using namespace std;
+long long int mod = 1e9 + 7;
+
+void solve()
+{
+    
+}
+
+int main()
+{
+    fast;
+    int t = 1;
+    while (t--)
+        solve();
+}
