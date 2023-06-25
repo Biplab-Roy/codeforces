9d40a408199d17c89ce92bbb57d3981335a40fa4
Biplab Roy <roybiplabofficial@gmail.com>
2023-06-25 22:04:16 +0530

    ds update

---
+++ DS_Biplab/pb_ds.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+             tree_order_statistics_node_update>
+    ordered_set;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
